bd932d80992d096ecb097dc24e6ace401657b5a5
Rlss91 <rlss91@gmail.com>
2022-07-12 20:06:10 +0300

    data types

---
+++ Students Project/js/data types.js
@@ -0,0 +1,15 @@
+console.log(1 + 1);
+console.log("1 + 1");
+
+let x = 1 + 1;
+x = x + 5;
+
+console.log(x);
+
+const kavua = 3.14;
+
+let y = 5,
+  z = 8;
+
+let sum = x + y + z;
+console.log(sum);
