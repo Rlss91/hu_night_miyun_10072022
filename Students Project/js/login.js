2d96f534ca7b0136ac89b3e59be87194f8a4cad6
Rlss91 <rlss91@gmail.com>
2022-07-17 21:03:23 +0300

    register

--- Students Project/js/login.js
+++ Students Project/js/login.js
@@ -1,11 +1,43 @@
-console.log("Hello world!");
+const emailFromServer = "kenny@gmail.com";
+const passwordFromServer = "Aa12345678";
+// console.log("Hello world!");
 // alert("Hello world!");
 
 //this is how we define a function
 //the name of the function is handleBtnClick()
 function handleBtnClick() {
-  console.log("clicked");
-  console.log(5 + 5);
+  //this command will give us the value from the input that has the id inside the ()
+  let emailValue = document.getElementById("emailInput").value;
+  // console.log("emailValue", emailValue);
+  //display in the console the value of the password
+  let passwordValue = document.getElementById("passwordInput").value;
+  // console.log("passwordValue", passwordValue);
+  // console.log(
+  //   "Your email is " + emailValue + ", your password is " + passwordValue
+  // );
+  // console.log(`Your email is ${emailValue}, your password is ${passwordValue}`);
+  // if (emailValue == emailFromServer) {
+  //   console.log("The email is correct");
+  // } else {
+  //   console.log("The email is incorrect");
+  // }
+  // if (passwordValue == passwordFromServer) {
+  //   console.log("The password is correct");
+  // } else {
+  //   console.log("The password is incorrect");
+  // }
+  /*
+    (emailValue == emailFromServer) => the email we got from the user is the same email that we got from the server
+    && => and
+    (passwordValue == passwordFromServer) => the password we got from the user is the same password that we got from the server
+  */
+  if (emailValue == emailFromServer && passwordValue == passwordFromServer) {
+    //the email and the password is correct
+    console.log("you can access to our website");
+    window.location.href = "register.html";
+  } else {
+    console.log("you shall not pass");
+  }
 }
 
 // handleBtnClick();
